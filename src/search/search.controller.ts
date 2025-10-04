c82aa1275246b018065d564b4a3b8b433e557776
Eric Scheel <119896592+agronauthdgame@users.noreply.github.com>
2025-10-04 17:26:31 +0200

    Fix the Search ( Removed the /path )

--- src/search/search.controller.ts
+++ src/search/search.controller.ts
@@ -10,10 +10,11 @@ import { SearchService } from './search.service';
 @Controller('search')
 export class SearchController {
   constructor(private readonly svc: SearchService) {}
-    @Post('path')
+    @Post()
     async search(@Body('q') query: string) {
       return this.svc.search(query);
     }
+    
     @Get('suggestions')
     async suggest(@Query('q') query: string) {
       return this.svc.suggest(query);
